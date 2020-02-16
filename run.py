e3e1b78c6cc448fa2e610a2ff4c985f0b30796bb
Chen Z <chenzhu@ucsb.edu>
2020-02-16 13:33:22 -0800

    Start working on the code structure

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import time
+import os
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('config/clients.xml')
+root = tree.getroot()
+
+for neighbor in root.iter('client'):
+	command = "osascript -e 'tell application \"Terminal\" to do script \"cd ~/Documents/CMPSC_271/Blockchain-Via-Paxos/ && python ./ini_blockchain.py " + neighbor.find('port').text + " " + neighbor.find('process_id').text + " " + neighbor.find('name').text + " " + neighbor.find('ip').text + " " + " \"' "
+	os.system(command)
+	time.sleep(3)
+	
